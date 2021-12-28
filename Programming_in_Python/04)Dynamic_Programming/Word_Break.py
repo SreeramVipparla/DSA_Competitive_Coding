95055ecbf4e4eed9ad1aa119d7e2c74b20b4aed9
VipparlaSreeram <vipparlasreeram2003@hotmail.com>
2021-12-28 14:49:39 +0400

    updated

---
+++ Programming_in_Python/04)Dynamic_Programming/Word_Break.py
@@ -0,0 +1,41 @@
+"""
+QUESTION-
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+"""
+"""
+ANSWER-
+"""
+class Solution:
+  def wordBreak(self, s, wordDict):
+    dp = [False] * (len(s)+1)
+    dp[0] = True
+    for i in xrange(1, len(s)+1):
+        for j in xrange(i):
+            if dp[j] and s[j:i] in wordDict:
+                dp[i] = True
+                break
+    return dp[-1]
+"""
+SOURCE-LEETCODE
+"""
---
+++ Programming_in_Python/04)Dynamic_Programming/World_Break_2.py
@@ -0,0 +1,68 @@
+"""
+QUESTION-
+
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+Example 2:
+
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+"""
+"""
+ANSWER-
+"""
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        return self.dfs(s, set(wordDict), {})
+    
+    def dfs(self, s, d, m):
+        if s in m: # memorize
+            return m[s]
+        if not s:
+            return [""]
+        res = []
+        for i in range(1, len(s)+1):
+            if s[:i] in d:
+                for word in self.dfs(s[i:], d, m):
+                    res.append(s[:i] + (" " if word else "") + word)
+        m[s] = res
+        return res
+    
+    def wordBreak1(self, s, wordDict):
+        res = []
+        self.dfs(s, set(wordDict), "", res)
+        return res
+    
+    def dfs1(self, s, dic, path, res):
+        if self.check(s, dic):
+            if not s:
+                res.append(path[1:])
+            for i in range(1, len(s)+1):
+                if s[:i] in d:
+                    self.dfs(s[i:], dic, path+" "+s[:i], res)
+                
+    def check(self, s, dic):
+        dp = [False] * (1+len(s))
+        dp[0] = True
+        for j in range(1, len(s)+1):
+            for i in range(j):
+                if dp[i] and s[i:j] in dic:
+                    dp[j] = True
+                    break
+        return dp[-1]
+"""
+SOURCE-LEETCODE+
+"""

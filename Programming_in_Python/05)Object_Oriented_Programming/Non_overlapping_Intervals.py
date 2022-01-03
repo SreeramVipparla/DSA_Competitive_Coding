949bfb3afaa1eecd36c1056c46ae0436b52c9f96
VipparlaSreeram <vipparlasreeram2003@hotmail.com>
2022-01-03 22:35:55 +0400

    updated

---
+++ Programming_in_Python/05)Object_Oriented_Programming/Non_overlapping_Intervals.py
@@ -0,0 +1,47 @@
+"""
+QUESTION-
+Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 105
+intervals[i].length == 2
+-5 * 104 <= starti < endi <= 5 * 104
+"""
+"""
+ANSWER-
+"""
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        res = 0
+        end = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < end:
+                end = min(end, intervals[i][1])
+                res += 1
+            else:
+                end = intervals[i][1]
+        return res
+"""
+SOURCE-LEETCODE
+"""
---
+++ Programming_in_Python/05)Object_Oriented_Programming/Number_of_Boomerangs.py
@@ -0,0 +1,59 @@
+"""
+QUESTION-
+You are given n points in the plane that are all distinct, where points[i] = [xi, yi]. A boomerang is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+
+Return the number of boomerangs.
+
+ 
+
+Example 1:
+
+Input: points = [[0,0],[1,0],[2,0]]
+Output: 2
+Explanation: The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]].
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: 2
+Example 3:
+
+Input: points = [[1,1]]
+Output: 0
+ 
+
+Constraints:
+
+n == points.length
+1 <= n <= 500
+points[i].length == 2
+-104 <= xi, yi <= 104
+All the points are unique.
+Accepted
+79,163
+
+"""
+"""
+ANSWER-
+"""
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        if len(points) < 3:
+            return 0
+        
+        res = 0
+       
+        for p1 in points:
+            tmp = {}
+            for p2 in points:
+                d = self.dist(p1, p2)
+                tmp[d] = tmp.get(d, 0) + 1
+            for val in [val for val in tmp.values() if val > 1]:
+                res += val * (val - 1)
+                
+        return res
+        
+    def dist(self, p1, p2):
+        return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+"""
+SOURCE-LEETCODE
+"""

5fd6e3bd673d923c33238965b32afe16a9a04de2
VipparlaSreeram <vipparlasreeram2003@hotmail.com>
2022-02-08 22:32:18 +0400

    first commit

---
+++ Programming_in_Python/01)Arrays/Add_Digits.py
@@ -0,0 +1,38 @@
+"""
+QUESTION-
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+"""
+"""
+ANSWER-
+"""
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
+"""
+SOURCE-LEETCODE
+"""

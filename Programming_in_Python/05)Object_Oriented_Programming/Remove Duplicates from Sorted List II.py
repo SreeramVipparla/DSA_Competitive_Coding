fb75373aca7119e94673cc4a59431c06eb1529ed
your_github_username <your_github_email>
2022-03-09 23:46:17 +0530

    updated

---
+++ Programming_in_Python/05)Object_Oriented_Programming/Remove Duplicates from Sorted List II.py
@@ -0,0 +1,54 @@
+"""
+QUESTION-
+Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.
+
+ 
+
+Example 1:
+
+Input: s = "owoztneoer"
+Output: "012"
+Example 2:
+
+Input: s = "fviefuro"
+Output: "45"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is one of the characters ["e","g","f","i","h","o","n","s","r","u","t","w","v","x","z"].
+s is guaranteed to be valid.
+"""
+"""
+ANSWER-
+"""
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        # sentinel
+        sentinel = ListNode(0, head)
+
+        # predecessor = the last node 
+        # before the sublist of duplicates
+        pred = sentinel
+        
+        while head:
+            # if it's a beginning of duplicates sublist 
+            # skip all duplicates
+            if head.next and head.val == head.next.val:
+                # move till the end of duplicates sublist
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                # skip all duplicates
+                pred.next = head.next 
+            # otherwise, move predecessor
+            else:
+                pred = pred.next 
+                
+            # move forward
+            head = head.next
+            
+        return sentinel.next
+"""
+SOURCE-LEETCODE
+"""

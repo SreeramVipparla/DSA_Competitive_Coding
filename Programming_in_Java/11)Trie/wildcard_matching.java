ecc357f573bf7587a4a88622f28e9af5aedf6984
Sreeram Vipparla <86887626+SreeramVipparla@users.noreply.github.com>
2022-03-14 23:18:03 +0530

    Create wildcard_matching.java

---
+++ Programming_in_Java/11)Trie/wildcard_matching.java
@@ -0,0 +1,52 @@
+"""
+QUESTION-
+Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+
+The objective of the game is to end with the most stones. The total number of stones across all the piles is odd, so there are no ties.
+
+Alice and Bob take turns, with Alice starting first. Each turn, a player takes the entire pile of stones either from the beginning or from the end of the row. This continues until there are no more piles left, at which point the person with the most stones wins.
+
+Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins.
+
+Example 1:
+
+Input: piles = [5,3,4,5]
+Output: true
+Explanation: 
+Alice starts first, and can only take the first 5 or the last 5.
+Say she takes the first 5, so that the row becomes [3, 4, 5].
+If Bob takes 3, then the board is [4, 5], and Alice takes 5 to win with 10 points.
+If Bob takes the last 5, then the board is [3, 4], and Alice takes 4 to win with 9 points.
+This demonstrated that taking the first 5 was a winning move for Alice, so we return true.
+Example 2:
+
+Input: piles = [3,7,2,3]
+Output: true
+ 
+
+Constraints:
+
+2 <= piles.length <= 500
+piles.length is even.
+1 <= piles[i] <= 500
+sum(piles[i]) is odd.
+"""
+"""
+ANSWER-
+"""
+from functools import lru_cache
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        @lru_cache(None)
+        def fn(i, j): 
+            """Return more piles by choosing from piles[i:j+1]"""
+            if i == j: return piles[i]
+            return max(piles[i] - fn(i+1, j), piles[j] - fn(i, j-1))
+        
+        return fn(0, len(piles)-1) > 0
+
+"""
+SOURCE-PEPCODING
+"""
